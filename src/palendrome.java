59c7bacafe18a264b35d57bb0867a99328d0e271
pr3d1t0r <ugochukwuanosike1@gmail.com>
2024-05-28 15:19:09 +0100

    first commit

---
+++ src/palendrome.java
@@ -0,0 +1,24 @@
+
+public class palendrome {
+    public static boolean isPalendrome(String str) {
+        str = str.replace("\\s+", "").toLowerCase();
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String string = "wow";
+        if (isPalendrome(string)) {
+            System.out.println(string + " is a palindrome");
+        } else {
+            System.out.println(string + " is not a palindrome");
+        }
+    }
+}
